cfb553d793b8c7c97ddcc3235b0b27d7e2885000
Rodrigo Mercê <35880673+rodrigomerce@users.noreply.github.com>
2021-08-25 23:32:52 +0000

    Ocultando senhas

--- main.py
+++ main.py
@@ -15,19 +15,27 @@ import sys
 import sqlite3
 import emoji
 from pytz import timezone
+import threading 
+from cryptography.fernet import Fernet
+import os
 
-geckodriver_autoinstaller.install()
-bot = telepot.Bot('1122143867:AAHcjx_Os2aFmEiaqcHFFAga2EfDL9L8Spo')
 
 global chat_ids
-#chat_id = '333563564'
-channel_id = "-1001595806705"
 
+my_secret = os.environ['senha']
+my_secret_chave_bot = os.environ['chave_bot']
+my_secret_id_canal = os.environ['id_canal']
+my_secret_id_meu_telegram = os.environ['id_meu_telegram']
+my_secret_id_telegram_neis = os.environ['id_telegram_neis']
+
+geckodriver_autoinstaller.install()
+
+bot = telepot.Bot(my_secret_chave_bot)
+
+channel_id = my_secret_id_canal
 chat_ids = [
-  '333563564','1051745745'
+  my_secret_id_meu_telegram, my_secret_id_telegram_neis
 ]
-
-
 
 
 def enviar():
@@ -36,15 +44,16 @@ def enviar():
 	bot.sendMessage(chat_id, 'Foi', parse_mode='Markdown')
 
 
-def DeletarTab():
+def Deletar(id):
   conn = sqlite3.connect('python.db')
   c = conn.cursor()
   #c.execute('DROP TABLE Agenda')
-  c.execute('DELETE FROM Agenda')
+  print(id)
+  c.execute('DELETE FROM Agenda WHERE ID='+ id )
   c.close
   conn.commit()
   conn.close
-  bot.sendMessage(chat_id, "Você apagou toda a base de dados",parse_mode="Markdown")
+  bot.sendMessage(chat_id, "Você apagou o lembrete: "+ id ,parse_mode="Markdown")
 
 
 def CriarTabela():
@@ -77,9 +86,12 @@ def Selecionar():
   rows = cur.fetchall()
   lembrete = ""
   for row in rows:
-    lembrete = lembrete + str(row[0]) + str(" - ") + row[1] + "\n"
-    #print(row)
-  bot.sendMessage(chat_id, lembrete, parse_mode="Markdown")
+    print(row)
+    lembrete = lembrete + str(row[0]) + " - " + str(row[1]) + "\n"
+
+  if lembrete != '':
+    bot.sendMessage(chat_id, str(lembrete), parse_mode="Markdown")
+    
   cur.close
   c.close
   conn.close
@@ -104,12 +116,18 @@ def handle(msg):
     elif text1[1:7] == "Agenda":
       Selecionar()
     elif text1[1:8] == "Deletar":
-      #ItensDeletar()
-      DeletarTab()
+      ItensDeletar()
+      #DeletarTab()
     elif text1[1:7] == "Ativos":
       Ativos()
+    elif text1[1:4] == "Loop":
+      Ativos()
+    elif text1[0:6] == "Senha:":
+      global senha
+      senha = text1[6:500]
+    elif text1[0:4] == 'Del:':
+      Deletar(text1[4:7])
     else:
-      print("InserirDados")
       Inserir(str(text1[0:500]))
       Selecionar()
 
@@ -121,8 +139,10 @@ MessageLoop(bot, handle).run_as_thread()
 
 from selenium.webdriver.firefox.options import Options
 
+
+
 options = Options()
-#options.headless = True
+options.headless = True
 driver = webdriver.Firefox(options=options)
 
 def ItensDeletar():
@@ -135,11 +155,12 @@ def ItensDeletar():
   lembrete = ""
   for row in rows:
     lembrete = str(row[0]) + str(" - ") + row[1]
-    bot.sendMessage(chat_id,'-',reply_markup = InlineKeyboardMarkup(inline_keyboard=[
-                                    [InlineKeyboardButton(text=emoji.emojize(":counterclockwise_arrows_button: "+lembrete+" "),callback_data='I have Nothing To do'),
+    bot.sendMessage(chat_id,'___________________________________________________',reply_markup = InlineKeyboardMarkup(inline_keyboard=[
+                                    [InlineKeyboardButton(text=emoji.emojize("Del:"+str(lembrete)+" "),callback_data=str(lembrete[0:10])),
                                 ]]
                             ))
     #print(row)
+
   cur.close
   c.close
   conn.close
@@ -155,7 +176,29 @@ def Ativos():
   Valores = Valores + "Valor do DODGE :*R$" + str(valor) + "*\n"
   bot.sendMessage(chat_id, Valores, parse_mode="Markdown")
 
+
+def Looop():
+  while loop == True:
+    valor = 0
+    driver.get("https://dolarhoje.com/iota/")
+    valor = driver.find_element_by_id("nacional").get_attribute('value')
+    if (float(valor.replace(',', '.')) >= 10.50):
+      for chat_id in chat_ids:
+        Valores = "*ATENÇÃO*\n\nValor da IOTA :*R$" + str(valor) + ", valor bom para a venda*\n"
+        bot.sendMessage(chat_id, Valores, parse_mode="Markdown")
+
+    driver.get("https://dolarhoje.com/dogecoin-hoje/")
+    valor = driver.find_element_by_id("nacional").get_attribute('value')
+    if (float(valor.replace(',', '.')) >= 1.95):
+      for chat_id in chat_ids:
+        Valores = "*ATENÇÃO*\n\nValor do DODGE :*R$" + str(valor) + ", valor bom para a venda*\n"
+        bot.sendMessage(chat_id, Valores, parse_mode="Markdown")
+    time.sleep(3600)
+
 def VerificaProvas():
+  if senha == "":
+    bot.sendMessage(chat_id, "Envie a senha", parse_mode="Markdown")
+    return
   global PrimeiroLogin
   bot.sendMessage(chat_id, "Verificando suas provas", parse_mode="Markdown")
   driver.get("https://apps.univesp.br/minhasprovas/")
@@ -167,7 +210,7 @@ def VerificaProvas():
     email.send_keys("1806217@aluno.univesp.br")
     email.send_keys(Keys.RETURN)
     senhas = driver.find_element_by_name("password")
-    senhas.send_keys("76577614@Cpu")
+    senhas.send_keys(senha)
     senhas.send_keys(Keys.RETURN)
     time.sleep(2)
   datas = driver.find_elements_by_css_selector(
@@ -216,11 +259,21 @@ def MensagemInicial():
           KeyboardButton(text=emoji.emojize(":money_bag:Ativos")),
           KeyboardButton(
               text=emoji.emojize(":counterclockwise_arrows_button:Deletar")),
+          KeyboardButton(
+              text=emoji.emojize(":counterclockwise_arrows_button:Loop")),
       ]]))
 
 HoraLembrete = '17:47:00'
 HoraLembrete = '17:47:02'
 
+global loop
+loop = True
+t1 = threading.Thread (target = Looop) 
+t1.start() 
+
+
+
+senha = my_secret
 
 while True:
   Agora = datetime.now(timezone('Brazil/East'))
